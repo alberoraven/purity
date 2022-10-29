4da47269a69e8c383b034639d94750ff4ce37fb1
Suresh Kumar <89379414+sureshkumar-SJ@users.noreply.github.com>
2022-10-29 18:17:17 +0530

    Profile page added (#2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,10 @@ const routes: Routes = [
     path: 'tutorial',
     loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
     canLoad: [CheckTutorial]
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),
   }
 ];
 
