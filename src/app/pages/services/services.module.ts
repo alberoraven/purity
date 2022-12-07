6afd82728a72347390eb0ba82171390672961927
Suresh Kumar <89379414+sureshkumar-SJ@users.noreply.github.com>
2022-12-07 18:00:16 +0530

    Feature/newpage (#10)
    * initial commit
    
    * initial commit
    
    * Nhost backend configured
    
    * apollo graphql added
    
    * Profile page added (#2)
    
    * Home and service page added
    
    * Home and service page added
    
    * package added
    
    * login page ui added
    
    * hame page ui added
    
    * forget page added
    
    * forget page link added
    
    * animation and forget page added and profile page ui updated
    
    * Few more pages added
    
    * service booking form added
    
    Co-authored-by: Rexmarkh <rexmarkh55@gmail.com>

--- src/app/pages/services/services.module.ts
+++ src/app/pages/services/services.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 
@@ -14,7 +14,8 @@ import { ServicesComponent } from './services.component';
     CommonModule,
     IonicModule,
     FormsModule,
-    ServicesRoutingModule
+    ServicesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ServicesModule { }
